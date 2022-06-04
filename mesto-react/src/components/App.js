b5bce12330933409c9d17e0b32aaa39f147ee22b
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-04 19:08:37 +0500

    feat: add button close listener

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -5,38 +5,51 @@ import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 
-const handleEditAvatarClick = () => {
-  document.querySelector('.popup_avatar').classList.add('popup_opened');
-};
-const handleEditProfileClick = () => {
-  document.querySelector('.popup_user').classList.add('popup_opened');
-};
-const handleAddPlaceClick = () => {
-  document.querySelector('.popup_card').classList.add('popup_opened');
-};
-
 function App() {
+  const [isEditAvatarPopupOpen, setIsAvatarPopup] = React.useState();
+  const [isEditProfilePopupOpen, setIsProfilePopup] = React.useState();
+  const [isAddPlacePopupOpen, setIsPlacePopup] = React.useState();
+  const [isClosePopup, ClosePopup] = React.useState();
+
+  function handleClickEditAvatar() {
+    setIsAvatarPopup(true);
+  }
+
+  function handleClickEditProfile() {
+    setIsProfilePopup(true);
+  }
+
+  function handleClickAddPlace() {
+    setIsPlacePopup(true);
+  }
+
+  function closeAllPopups() {
+    setIsAvatarPopup(false);
+    setIsProfilePopup(false);
+    setIsPlacePopup(false);
+  }
+    
+
+
   return (
     <body className="page">
       <div className="content">
-
         <Header />
-        <Main onEditAvatar={handleEditAvatarClick} onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}/>
+        <Main onEditAvatar={handleClickEditAvatar} onEditProfile={handleClickEditProfile} onAddPlace={handleClickAddPlace}/>
         <Footer />
-
-        <PopupWithForm name="avatar" title="Обновить аватар" isOpen>
+        <PopupWithForm name="avatar" title="Обновить аватар" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
           <input id="link-avatar" className="popup__input popup__input_field_avatar-link" type="url" name="Link" placeholder="Ссылка на картинку" required />
           <span id="link-avatar-error" className="popup__input-error popup__input-error_number_one"></span>
           <button className="popup__button-add popup__button-add_avatar" type="submit">Сохранить</button>
         </PopupWithForm>
-        <PopupWithForm name="user" title="Редактировать профиль" isOpen>
+        <PopupWithForm name="user" title="Редактировать профиль" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
           <input id="name" className="popup__input popup__input_field_name" type="text" name="Name" placeholder="Имя" minlength="2" maxlength="40" required />
           <span id="name-error" className="popup__input-error popup__input-error_number_one"></span>
           <input id="job" className="popup__input popup__input_field_job" type="text" name="Job" placeholder="Профессиональная деятельность" minlength="2" maxlength="200" required />
           <span id="job-error" className="popup__input-error popup__input-error_number_two"></span>
           <button className="popup__button-add popup__button-add_user" type="submit">Сохранить</button>
         </PopupWithForm>
-        <PopupWithForm name="card" title="Новое место" isOpen>
+        <PopupWithForm name="card" title="Новое место" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
           <input id="place" className="popup__input popup__input_field_place" type="text" name="Place" minlength="2" maxlength="30" placeholder="Название" required />
           <span id="place-error" className="popup__input-error popup__input-error_number_one"></span>
           <input id="link" className="popup__input popup__input_field_link" type="url" name="Link" placeholder="Ссылка на картинку" required />
@@ -46,7 +59,6 @@ function App() {
         <PopupWithForm name="confirm" title="Вы уверены?">
           <button className="popup__button-add popup__button-add_confirm" type="submit">Да</button>
         </PopupWithForm>
-
         <ImagePopup />
 
         <template className="element-template">
--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 function PopupWithForm(props) {
-  return (
-    <div className={`popup popup_${props.name}`}>
-      <div className={`popup__container popup__container_${props.name} container`}>
-        <button className="popup__button-close" type="button"></button>
-        <div className="popup__content">
-          <h2 className="popup__title">{props.title}</h2>
-          <form className={`popup__form popup__form_${props.name}`} name="Form" novalidate>
-            {props.children}
-          </form>
+  if (props.isOpen) {
+    return (
+      <div className={`popup popup_${props.name} popup_opened`}>
+        <div className={`popup__container popup__container_${props.name} container`}>
+          <button className="popup__button-close" type="button" onClick={props.onClose}></button>
+          <div className="popup__content">
+            <h2 className="popup__title">{props.title}</h2>
+            <form className={`popup__form popup__form_${props.name}`} name="Form" novalidate>
+              {props.children}
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }  
 };
 
 export default PopupWithForm;
