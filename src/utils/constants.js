dff7820b5553456c701042023d421debd6a31122
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-17 15:15:54 +0500

    fix: deleted constants.js

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const validateSetting = {
-  form: '.popup__form',
-  input: '.popup__input',
-  buttonAddPopup: '.popup__button-add',
-  buttonAddPopupInvalid: 'popup__button-add_invalid',
-  buttonAddPopupHover: 'popup__button-add_hover',
-  inputInvalid: 'popup__input_invalid',
-};
